32ede2c603ee4d8165eb4b4b23d39670bb1bf98d
rezdenSSPS <rezdencsgo@gmail.com>
2025-08-05 11:56:50 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,29 +4,31 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { CurrencyProvider } from "./context/CurrencyContext";
+import { AuthProvider } from "./context/AuthContext"; // <-- IMPORTUJTE AUTH PROVIDER
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import GamePage from "./pages/GamePage";
-import AllGamesPage from "./pages/AllGamesPage"; // Import nové stránky
+import AllGamesPage from "./pages/AllGamesPage";
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <CurrencyProvider>
-      <TooltipProvider>
-        <Toaster />
-        <Sonner />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/games" element={<AllGamesPage />} /> {/* Přidána cesta pro stránku se všemi hrami */}
-            <Route path="/game/:slug" element={<GamePage />} />
-            {/* VŠECHNY VLASTNÍ CESTY PŘIDÁVEJTE NAD TENTO ŘÁDEK */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </TooltipProvider>
+      <AuthProvider> {/* <-- OBALTE APLIKACI */}
+        <TooltipProvider>
+          <Toaster />
+          <Sonner />
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Index />} />
+              <Route path="/games" element={<AllGamesPage />} />
+              <Route path="/game/:slug" element={<GamePage />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </BrowserRouter>
+        </TooltipProvider>
+      </AuthProvider> {/* <-- UZAVŘETE OBAL */}
     </CurrencyProvider>
   </QueryClientProvider>
 );

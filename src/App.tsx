a94ddad19f594661c99d003fa20a60f7ddfaa993
rezdenSSPS <rezdencsgo@gmail.com>
2025-08-05 12:20:09 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { CurrencyProvider } from "./context/CurrencyContext";
-import { AuthProvider } from "./context/AuthContext"; // <-- IMPORTUJTE AUTH PROVIDER
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import GamePage from "./pages/GamePage";
@@ -15,20 +14,18 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <CurrencyProvider>
-      <AuthProvider> {/* <-- OBALTE APLIKACI */}
-        <TooltipProvider>
-          <Toaster />
-          <Sonner />
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Index />} />
-              <Route path="/games" element={<AllGamesPage />} />
-              <Route path="/game/:slug" element={<GamePage />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </BrowserRouter>
-        </TooltipProvider>
-      </AuthProvider> {/* <-- UZAVŘETE OBAL */}
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/games" element={<AllGamesPage />} />
+            <Route path="/game/:slug" element={<GamePage />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
     </CurrencyProvider>
   </QueryClientProvider>
 );
